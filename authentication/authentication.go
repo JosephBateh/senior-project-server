6f3107bf57f500a3434c65d5faa9f0620509d139
Joseph Bateh <josephbateh@gmail.com>
2017-10-24 10:13:32 -0400

    Login constantly listening
    Login is constantly listening as a goroutine. This is made possible by the waitgroup.

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -1,49 +1,50 @@
-// This example demonstrates how to authenticate with Spotify using the authorization code flow.
-// In order to run this example yourself, you'll need to:
-//
-//  1. Register an application at: https://developer.spotify.com/my-applications/
-//       - Use "http://localhost:8080/callback" as the redirect URI
-//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
-//  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
 package authentication
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"github.com/zmb3/spotify"
 )
 
-// redirectURI is the OAuth redirect URI for the application.
-// You must register an application at Spotify's developer portal
-// and enter this value.
 const redirectURI = "http://localhost:8080/callback"
 
 var (
-	auth  spotify.Authenticator
-	ch    = make(chan *spotify.Client)
-	state = "abc123"
+	auth      spotify.Authenticator
+	waitGroup sync.WaitGroup
+	ch        = make(chan *spotify.Client)
+	state     = "abc123"
 )
 
 // Start starts the process of listening for authentication requests
 func Start() {
-	// Load environment variables before anything else.
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
-	auth = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	loadEnv()
+
+	waitGroup.Add(1)
+	go listen()
+	fmt.Println("Searver listening...")
+	waitGroup.Wait()
+
+}
+
+func listen() {
+	auth = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate, spotify.ScopePlaylistReadPrivate, spotify.ScopePlaylistModifyPublic, spotify.ScopePlaylistModifyPrivate, spotify.ScopePlaylistReadCollaborative, spotify.ScopeUserLibraryModify, spotify.ScopeUserLibraryRead, spotify.ScopeUserReadPrivate, spotify.ScopeUserReadCurrentlyPlaying, spotify.ScopeUserReadPlaybackState, spotify.ScopeUserModifyPlaybackState, spotify.ScopeUserReadRecentlyPlayed, spotify.ScopeUserTopRead)
+
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", login)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	waitGroup.Done()
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Login...")
 	url := auth.AuthURL(state)
 	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
-
-	// first start an HTTP server
-	http.HandleFunc("/callback", completeAuth)
-	http.HandleFunc("/", startAuth)
-	go http.ListenAndServe(":8080", nil)
-
 	// wait for auth to complete
 	client := <-ch
 
@@ -53,10 +54,6 @@ func Start() {
 		log.Fatal(err)
 	}
 	fmt.Println("You are logged in as:", user.ID)
-}
-
-func startAuth(w http.ResponseWriter, r *http.Request) {
-	log.Println("Got request for:", r.URL.String())
 }
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
@@ -73,4 +70,11 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 	client := auth.NewClient(tok)
 	fmt.Fprintf(w, "Login Completed!")
 	ch <- &client
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
